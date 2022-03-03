4e31e19f0b4b244b92ee9d2382941489059bbec6
Michael Kim <michaelkim1789@yandex.ru>
2022-03-03 17:07:17 +0300

    [sprint_2] revert test changes

--- server.js
+++ server.js
@@ -3,11 +3,9 @@ const app = express();
 const PORT = 3000;
 const build = `${__dirname}/build`
 
-function serverIndex(req, res) {
-  return res.sendFile(`${build}/index.html`);
-}
-
 app.use(express.static(build));
-app.get('*', serverIndex);
+app.get('*', (req, res) => {
+  return res.sendFile(`${build}/index.html`);
+});
 
 app.listen(PORT, () => {});
