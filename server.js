553c723b5bcce029b95e7035492db362d85ba97e
Michael Kim <michaelkim1789@yandex.ru>
2022-03-03 14:43:41 +0300

    [sprint_2] change express file and run scripts

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const app = express();
 const PORT = 3000;
 const build = `${__dirname}/build`
 
+function serverIndex(req, res) {
+  return res.sendfile(`${build}/index.html`);
+}
+
 app.use(express.static(build));
+app.get('*', serverIndex);
 
 app.listen(PORT, () => {});
